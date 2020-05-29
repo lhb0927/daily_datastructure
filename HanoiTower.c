e51a2295f107dbffa78fcd02f1e00a6e3463a97f
Lim hyeongbin <lhb0927@naver.com>
2020-05-29 12:14:28 +0900

    Add HanoiTower.c

---
+++ HanoiTower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void HanoiTowerMove(int num, char from, char by, char to)
+{
+	if(num ==1)
+		printf("원반1을 %c에서 %c로 이동!!\n",from,to);
+	else
+	{
+		HanoiTowerMove(num-1,from,to,by);
+		printf("원반%d을(를) %c에서 %c로 이동!!\n",num,from,to);
+		HanoiTowerMove(num-1,by,from,to);
+	}
+}
+
+int main()
+{
+	int count =0;
+	printf("원반의 갯수 : ");
+	scanf("%d",&count);
+
+	HanoiTowerMove(count,'A','B','C');
+	return 0;
+}
