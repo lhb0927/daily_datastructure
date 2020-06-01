a7779758344be25b89e2bd7d50da7dede0e99029
Lim hyeongbin <lhb0927@naver.com>
2020-06-02 02:08:42 +0900

    Add ArrayList.h

---
+++ ArrayList.h
@@ -0,0 +1,29 @@
+#ifndef __ARRAY_LIST_HH
+#define __ARRAY_LIST_HH
+
+#define TRUE 1
+#define FALSE 0
+
+#define LIST_LEN 100
+
+typedef int LData;
+
+typedef struct __ArrayList
+{
+	LData arr[LIST_LEN];
+	int numOfData;
+	int curPosition;
+}ArrayList;
+
+typedef ArrayList List;
+
+void ListInit(List *plist);
+void LInsert(List *plist);
+
+int LFirst(List *plist,LData *pdata);
+int LNext(List * plist,LData *pdata);
+
+LData LRemove(List *plist);
+int LCount(List *plist);
+
+#endif
