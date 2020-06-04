0bd1659489ea88fe8ad8887b05737393dffbf603
Lim hyeongbin <lhb0927@naver.com>
2020-06-04 16:33:49 +0900

    Add ListMain.c

---
+++ ListMain.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "ArrayList.h"
+
+int main(void)
+{
+	//Initialization
+	List list;
+	int data;
+	ListInit(&list);
+
+	LInsert(&list,11);
+	LInsert(&list,11);
+	LInsert(&list,22);
+	LInsert(&list,22);
+	LInsert(&list,33);
+
+	printf("Data# : %d\n",LCount(&list));
+
+	if(LFirst(&list, &data))
+	{
+		printf("%d ",data);
+		
+		while(LNext(&listm &data))
+			printf("%d ",data);
+	}	
+	printf("\n\n");
+
+	if(LFirst(&list, &data))
+	{
+		if(data==22)
+			LRemove(&list);
+
+		while(LNext(&list, &data))
+		{
+			if(data==22)
+				LRmove(&list);
+		}
+	}
+
+	printf("Data# : %d\n",LCount(&list));
+
+	if(LFirst(&list,&data))
+	{
+		printf("%d ",data);
+
+		while(LNext(&list, &data))
+			printf("%d ",data);
+	}
+	printf("\n\n");
+	return 0;
+}
