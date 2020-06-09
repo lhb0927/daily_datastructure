c2aaecd13361162c3b43b4126a6a8272c4aafb76
Lim hyeongbin <lhb0927@naver.com>
2020-06-10 00:36:05 +0900

    Add CircularQueue.h

---
+++ Queue/CircularQueue.h
@@ -0,0 +1,21 @@
+#define TRUE 1
+#define FALSE 0
+
+#define QUE_LEN 100
+typedef int Data;
+
+typedef struct _cQueue
+{
+	int front;
+	int rear;
+	Data queArr[QUE_LEN];
+} CQueue;
+
+typedef CQueue Queue;
+
+void QueueInit(Queue *pq);
+int QIsEmpty(Queue *pa);
+
+void Enqueue(Queue *pq,Data data);
+Data Dequeue(Queue *pq);
+Data QPeek(Queue *pq);
