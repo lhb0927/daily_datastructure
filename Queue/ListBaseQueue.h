98925820a95be5ad267952e9f91a876f142d5c2c
Lim hyeongbin <lhb0927@naver.com>
2020-06-16 01:42:48 +0900

    Add ListBaseQueue.h

---
+++ Queue/ListBaseQueue.h
@@ -0,0 +1,25 @@
+#define TRUE 1
+#define FALSE 0
+
+typedef int Data;
+
+typedef struct _node
+{
+	Data data;
+	struct _node *next;
+} Node;
+
+typedef struct _lQueue
+{
+	Node *front;
+	Node *rear;
+} LQueue;
+
+typedef LQueue Queue;
+
+void QueueInit(Queue *pq);
+int QInEmpty(Queue *pq);
+
+void Enqueue(Queue *pq, Data data);
+Data Dequeue(Queue *pq);
+Data QPeek(Queue *pq);
