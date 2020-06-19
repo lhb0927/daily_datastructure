792018b578eab22e06fed9f6b03cae8f87a2bc22
Lim hyeongbin <lhb0927@naver.com>
2020-06-20 02:12:50 +0900

    Rename Deque.h

---
+++ Queue/Deque.h
@@ -0,0 +1,31 @@
+#define TRUE 1
+#define FALSE 0
+
+typedef int Data;
+
+typedef struct _node
+{
+	Data data;
+	struct _node *next;
+	struct _node *prev;
+} Node;
+
+typedef struct _dlDeque
+{
+	Node *head;
+	Node *tail;
+} DLDeque;
+
+typedef DLDeque Deque;
+
+void DequeInit(Deque *pdeq);
+int DQIsEmpty(Deque *pdeq);
+
+void DQAddFirst(Deque *pdeq, Data data);
+void DQAddLast(Deque *pdeq, Data data);
+
+Data DQRemoveFirst(Deque *pdeq);
+Data DQRemoveLast(Deque *pdeq);
+
+Data DQGetFirst(Deque *pdeq);
+Data DQGteFirst(Deque *pdeq);
